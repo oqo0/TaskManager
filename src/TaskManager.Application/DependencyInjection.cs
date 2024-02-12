f7401670a32e73913ddc144c2e1fbca5a67b96ec
oqo0 <oq.roman@gmail.com>
2024-02-12 23:41:01 +0300

    Inject mediatr and fluent validation

---
+++ src/TaskManager.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using MediatR;
+using System.Reflection;
+using FluentValidation;
+
+namespace TaskManager.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
